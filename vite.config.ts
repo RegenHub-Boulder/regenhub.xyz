88aa8c1382e172ec4d67e9567c13a09941285a3c
Aaron Gabriel Neyer <ag@unforced.org>
2025-09-23 18:57:09 -0600

    Switch to GitHub Pages default domain for testing

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE_URL || "/",
+  base: process.env.VITE_BASE_URL || "/regenhub.xyz/",
   server: {
     host: "::",
     port: 8080,
